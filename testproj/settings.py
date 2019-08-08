8333ee489fb4aa34b878191f6cbc39ad1f2d9c26
realshad <realshad07@gmail.com>
2019-08-08 11:39:54 +0530

    commited testproj1

--- testproj/settings.py
+++ testproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vzblw128*^vwkl03l^65n5u#d)e!%z6kkve_94!8*y*!dihvbu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
